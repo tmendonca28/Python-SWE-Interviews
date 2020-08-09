5ee7c64dfaa99562468642cc2e88a8b9598cc3be
tmendonca28 <tmendonca28@gmail.com>
2020-08-09 11:12:27 +0100

    Adds Balanced Parentheses Question using Stack implementation
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ Python-DS/Stack/stack_balanced_parens.py
@@ -0,0 +1,40 @@
+"""
+Use a stack to check whether or not a string
+has balanced usage of parentheses
+"""
+from stack import Stack
+
+def is_match(p1, p2):
+    if p1 == "(" and p2 == ")":
+        return True
+    elif p1 == "[" and p2 == "]":
+        return True
+    elif p1 == "{" and p2 == "}":
+        return True
+    else:
+        return False
+
+def is_paren_balanced(paren_string):
+    s = Stack()
+    is_balanced = True
+    index = 0
+
+    while index < len(paren_string) and is_balanced:
+        paren = paren_string[index]
+        if paren in "([{":
+            s.push(paren)
+        else:
+            if s.is_empty():
+                is_balanced = False
+            else:
+                top = s.pop()
+                if not  (top, paren):
+                    is_balanced = False
+        index += 1
+    if s.is_empty() and is_balanced:
+        return True
+    else:
+        return False
+
+
+print(is_paren_balanced("([}}])"))
