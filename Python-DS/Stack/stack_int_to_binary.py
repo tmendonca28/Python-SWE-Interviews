c3a3d751b5ae546276717cdbb019c0a27c6950b0
tmendonca28 <tmendonca28@gmail.com>
2020-08-09 19:49:41 +0100

    Adds Int to Bin
    Transforming int to binary using Stack DS.
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ Python-DS/Stack/stack_int_to_binary.py
@@ -0,0 +1,36 @@
+"""
+Use a stack data structure to convert integer to binary
+
+Example : 242
+
+242 / 2 = 121 -> 0
+121 / 2 = 60 -> 1
+60 / 2 = 30 -> 0
+30 / 2 = 15 -> 0
+15 / 2 = 7 -> 1
+7 / 2 = 3 -> 1
+3 / 2 = 1 -> 1
+1 / 2 = 0 -> 1
+
+Reading upwards gives us the binary repr : 11110010
+"""
+
+from stack import Stack
+
+
+def div_by_2(dec_num: int) ->  str:
+    s = Stack()
+
+    while dec_num > 0:
+        rem = dec_num % 2
+        s.push(rem)
+        dec_num = dec_num // 2
+
+    bin_num = ""
+    while not s.is_empty():
+        bin_num += str(s.pop())
+
+    return bin_num
+
+
+print(div_by_2(242))
