6c709867cafb79d6e1444a33551d8e2573bda193
tmendonca28 <tmendonca28@gmail.com>
2020-08-12 09:37:13 +0100

    Adds getAverage and removeList
    getAverage gets the average of a list.
    removeList removes sublist from list using list comprehension.
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

--- Python-DS/FullSpeedPython/lists.py
+++ Python-DS/FullSpeedPython/lists.py
@@ -3,3 +3,26 @@ def getSublist():
     l1 = l[:3]
     l2 = l[3:]
     return [l1, l2]
+
+
+def AppendToList():
+    l = [1, 4, 9, 10, 23]
+    l.append(90)
+    return l
+
+
+def getAverage():
+    l1 = [1, 4, 9, 10, 23]
+    avg = sum(l1) / len(l1)
+    return avg
+
+print(getAverage())
+
+
+
+def removeList():
+    l1 = [1, 4, 9, 10, 23]
+    l2 = [4, 9]
+    return [x for x in l1 if x not in l2]
+
+print(removeList())
