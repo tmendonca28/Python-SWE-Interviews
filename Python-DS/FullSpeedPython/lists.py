0b86c1903e191cb09471373210c4779d6e68187e
tmendonca28 <tmendonca28@gmail.com>
2020-08-12 14:29:45 +0100

    Adds getSquare function
    Gets squares of numbers in range 1-10 using list comprehension
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

--- Python-DS/FullSpeedPython/lists.py
+++ Python-DS/FullSpeedPython/lists.py
@@ -19,10 +19,17 @@ def getAverage():
 print(getAverage())
 
 
-
 def removeList():
     l1 = [1, 4, 9, 10, 23]
     l2 = [4, 9]
     return [x for x in l1 if x not in l2]
 
 print(removeList())
+
+
+def getSquare():
+    l1 = [x*x for x in list(range(1, 11))]
+    return l1
+
+
+print(getSquare())
