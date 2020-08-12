bcb73f1c024c350aee4f932e47a9e7d99e196941
tmendonca28 <tmendonca28@gmail.com>
2020-08-13 00:25:25 +0100

    Adds calculateSinCosTan and findMaximum
    Using inbuilt math fns to calc cos, sin and tan.
    Uses max() fn to find max of 2 nums.
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

--- Python-DS/FullSpeedPython/modules.py
+++ Python-DS/FullSpeedPython/modules.py
@@ -4,3 +4,19 @@ def findGCD(a, b):
     return math.gcd(a, b)
 
 print(findGCD(8, 12))
+
+
+def calculateSinCosTan(x):
+    sine = math.sin(x)
+    cos = math.cos(x)
+    tan = math.tan(x)
+    return [sine, cos, tan]
+
+
+print(calculateSinCosTan(0))
+
+
+def findMaximum(x ,y):
+    return max(x,y)
+
+print(findMaximum(2, 3))
