2fd81b49d94d0abf10ec51afec3217774170b96d
tmendonca28 <tmendonca28@gmail.com>
2020-08-13 15:59:31 +0100

    Adds findMaximum and find MaximumValueIndex
    Uses max() and index() functions on iterable
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

--- Python-DS/FullSpeedPython/iterations_and_loops.py
+++ Python-DS/FullSpeedPython/iterations_and_loops.py
@@ -3,3 +3,17 @@ def sumList(elem_list) -> int:
 
 
 print(sumList([1, 2, 3, 4, 5]))
+
+
+def findMaximum(l):
+    return max(l)
+
+print(findMaximum([1, 4, 9, 10, 23]))
+
+
+def findMaximumValueIndex(l):
+    index = l.index(max(l))
+    maximum = max(l)
+    return [index,maximum]
+
+print(findMaximumValueIndex([1, 4, 23, 10, 9]))
