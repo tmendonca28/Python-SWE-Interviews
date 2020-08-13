4ee1a567e1df14249a90011b30f4b9657af6527a
tmendonca28 <tmendonca28@gmail.com>
2020-08-13 16:33:24 +0100

    Adds iteration in Python
    isSorted shows whether list is sorted
    hasDuplicates tells whether the list has dupes using set
    printEvenOdd uses the reversed(range(..))
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

--- Python-DS/FullSpeedPython/iterations_and_loops.py
+++ Python-DS/FullSpeedPython/iterations_and_loops.py
@@ -17,3 +17,36 @@ def findMaximumValueIndex(l):
     return [index,maximum]
 
 print(findMaximumValueIndex([1, 4, 23, 10, 9]))
+
+
+def reverseList(l):
+    l.reverse()
+    return l
+
+
+print(reverseList(['Windows', 'macOS', 'Linux']))
+
+
+def isSorted(l):
+    return sorted(l) == l
+
+
+print(isSorted([1, 2, 2, 3]))
+
+
+def hasDuplicates(l):
+    # returns true if there are duplicates
+    return len(l) != len(set(l))
+
+
+print(hasDuplicates([1, 2, 2, 3]))
+
+
+def printEvenOdd(n):
+    for num in reversed(range(1, n+1)):
+        if num%2==0:
+            print(f'Even number: {num}')
+        else:
+            print(f'Odd number: {num}')
+
+printEvenOdd(10)
