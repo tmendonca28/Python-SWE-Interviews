2f3e76d128d041dd71fd1eba9071832bb1b82ce3
tmendonca28 <tmendonca28@gmail.com>
2020-08-22 23:14:55 +0100

    Adds Backtracking folder
    Practicing backtracking in Python.
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ SWE-Interview-Questions/Microsoft/Backtracking/letter_combinations_of_a_phone_number.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        mapping = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        result = []
+        self.dfs(digits, mapping, 0, "", result)
+        return result
+
+    def dfs(self, digits, mapping, index, path, result):
+        if len(path) == len(digits):
+            result.append(path)
+            return
+        for i in range(index, len(digits)):
+            for j in mapping[digits[i]]:
+                self.dfs(digits, mapping, i+1, path+j, result)
+
+    
+    if __name__== "__main__":
+        print(letterCombinations("23"))
