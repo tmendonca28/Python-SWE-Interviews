3b206cebde34f985631b736df8059687a8c8f16b
tmendonca28 <tmendonca28@gmail.com>
2020-10-12 09:57:49 +0300

    Adds Refresher folder
    Working through Python Essentials book from Foundations of Applied
    Mathematics
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ Refresher/itertoolsModule.py
@@ -0,0 +1,14 @@
+from itertools import chain, cycle, combinations, permutations
+
+first_list = ["abc"]
+
+chained_list = list(chain("abc", ['d', 'e'], ('f', 'g')))
+
+print(chained_list, end="\n")
+
+
+# Get all combinations of length 2 from iterable "ABC"
+print(list(combinations("ABC", 2)))
+
+# Get all permutations of length 2 from ABC
+print(list(permutations("ABC", 2)))
---
+++ Refresher/playingWithForLoop.py
@@ -0,0 +1,18 @@
+colors = ["red", "green", "blue", "yellow"]
+
+for color in colors:
+    print(color)
+
+print()
+for index in range(len(colors)):
+    print(colors[index])
+
+print()
+for i, v in enumerate(colors):
+    print(v)
+
+print()
+
+for color in reversed(colors):
+    print(color)
+    
