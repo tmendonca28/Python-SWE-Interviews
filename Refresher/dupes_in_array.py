c0e2d96183c533c70272f6434b1ecb1891dd8ff4
tmendonca28 <tmendonca28@gmail.com>
2020-10-22 17:58:21 +0300

    Adds duplicates in array
    Used count function of lists
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ Refresher/dupes_in_array.py
@@ -0,0 +1,13 @@
+"""
+Write a method that takes in a list of positive ints and
+returns list of element that appears twice in order of the
+first time it has been encountered
+"""
+
+def list_duplicate_numbers(arr : list) -> list:
+    dupes = []
+    
+    for i in arr:
+        if arr.count(i)>1 and i not in dupes:
+            dupes.append(i)
+    return dupes
