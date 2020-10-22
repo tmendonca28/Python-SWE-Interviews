3914db7b19c94cf381b256c679418f6d1817fe95
tmendonca28 <tmendonca28@gmail.com>
2020-10-22 12:23:57 +0300

    Adds ordered duplicates
    Uses set in a smart fashion
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ Refresher/ordered_duplicate_number.py
@@ -0,0 +1,11 @@
+"""
+Write a function that takes in an ordered
+sequence of numbers and returns either True or False depending on
+if a duplicate number can be found
+"""
+
+def is_duplicate_number(arr : list) -> bool:
+    unique = set(arr)
+    if len(arr) != len(unique):
+        return True
+    return False
