f8052325bf05c559ca76bf73002bff8503c1b0c8
tmendonca28 <tmendonca28@gmail.com>
2020-10-23 12:52:51 +0300

    Adds reversed string
    Using slicing.
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ Refresher/reversed_string.py
@@ -0,0 +1,10 @@
+"""
+Write a method that takes in a string and returns the reverse
+of that string.
+"""
+
+def reverse(input: str) -> str:
+    if input:
+        return input[::-1]
+    return "You Have Inputan Out of Bounds Value."
+    
